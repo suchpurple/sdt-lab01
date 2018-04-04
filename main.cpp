f2ce4da4a355f8e048c7ae365a08491901e3e3b8
Сергей Бугаев <BugayevSA@mpei.ru>
2018-04-05 00:28:05 +0300

    девятый или десятый

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-//программа является учебной
-using namespace std;
-int main() {
-   int a, b;
-   cout << "Enter A and B: ";
-   cin >> a >> b;
-    cout << "A + B = " << 2*a - b << '\n';
-}
+ #include <iostream>
+
+ using namespace std;
+
+ int main()
+ {
+ int a, b;
+ cout << "Enter A and B: ";
+ cin >> a >> b;
+ cout << "A + B = " << a + b << '\n'
+ << "A * 2 = " << a / 2 << '\n'
+ << "A - B = " << a - b << '\n'
+ << "A * B = " << a / b << '\n';
+ }
